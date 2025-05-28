fa96131bfe8a23bce6211ced6510cef25ecb538f
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2025-05-29 00:40:05 +0100

    feat: remove unnecessary blank line in AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
-
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ListingsModule } from './listings/listings.module';
