13f287087b881dad2071c634b2da5a67447a6fd6
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2025-06-10 21:25:58 +0100

    feat: enhance AuthController with login and profile retrieval methods, and improve signup method

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,44 @@
-import { Body, Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // مسار التسجيل (Sign Up)
+  @Post('signup')
   async signup(@Body() createUserDto: CreateUserDto) {
-    return this.authService.signup(createUserDto);
+    return await this.authService.signup(createUserDto);
+  }
+
+  // مسار تسجيل الدخول (Login) باستخدام الحارس المحلي (LocalAuthGuard)
+  @UseGuards(LocalAuthGuard) // هذا الحارس يحقّق البريد وكلمة المرور أولًا
+  @Post('login')
+  async login(@Request() req: { user: { email: string; password: string } }) {
+    // بعد نجاح الحارس، يكون req.user مُضمّنًا
+    return await this.authService.login({
+      email: req.user.email,
+      password: req.user.password, // تأكد من أن كلمة المرور موجودة في req.user
+    });
+  }
+
+  @UseGuards(JwtAuthGuard) // هذا الحارس يستخدم للتحقق من صلاحية المستخدمين باستخدام JWT
+  @Get('profile')
+  getProfile(@Request() req) {
+    return {
+      message: 'User profile retrieved successfully',
+      user: req.user ,
+      // يمكنك إضافة المزيد من المعلومات هنا إذا لزم الأمر
+    };
   }
 }
