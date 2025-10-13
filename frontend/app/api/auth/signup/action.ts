0a81cd545ec4c6596181d967baef17631ee251ee
Ab_Nas <116016564+nasrokamora@users.noreply.github.com>
2025-10-13 19:12:10 +0100

    Refactor SignUpAction and UserAction functions for improved readability and maintainability

--- frontend/app/api/auth/signup/action.ts
+++ frontend/app/api/auth/signup/action.ts
@@ -2,6 +2,7 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+// import { NextRequest } from "next/server";
 // import { cookies } from "next/headers";
 // import { redirect } from "next/navigation";
 
@@ -16,40 +17,42 @@ export async function SignUpAction(formData: FormData) {
     body: JSON.stringify({ username, email, password }),
     credentials: "include",
   });
-//   const data = await response.json();
-  if(formData.get("email") === email){
-    redirect("/auth/signup")
+  //   const data = await response.json();
+  if (formData.get("email") === email) {
+    redirect("/auth/signup");
   }
-
-// //   if (response.status === 409) {
-// //     redirect("/auth/login")
-// //   }
-  if(!response.ok){
-    throw new Error("Failed to sign up");
+  if (response.status === 409) {
+    redirect("/auth/login");
   }
   
+  // //   if (response.status === 409) {
+  // //     redirect("/auth/login")
+  // //   }
+  if (!response.ok) {
+    throw new Error("Failed to sign up");
+  }
 
-    redirect("/dashboard");
-    // return data;
+  redirect("/dashboard");
+  // return data;
 }
 
-export async function UserAction(){
-    const cookieStore = cookies();
-    const accessToken = (await cookieStore).get("access_token")?.value;
-    let user :{username?:string, email?:string} | null = null;
-    if(accessToken){
-        const response = await fetch(`${process.env.NEXT_NEST_API_URL}/auth/me`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${accessToken}`
-            },
-            credentials: "include",
-            cache: "no-store"
-        });
-        if(response.ok){
-            user = await response.json();
-        }
-        return user;
+export async function UserAction() {
+  const cookieStore = cookies();
+  const accessToken = (await cookieStore).get("access_token")?.value;
+  let user: { username?: string; email?: string } | null = null;
+  if (accessToken) {
+    const response = await fetch(`${process.env.NEXT_NEST_API_URL}/auth/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      credentials: "include",
+      cache: "no-store",
+    });
+    if (response.ok) {
+      user = await response.json();
     }
+    return user;
+  }
 }
